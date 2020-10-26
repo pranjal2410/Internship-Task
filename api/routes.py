e8959b7c9272a5a4e32c60749a36cb709d8249c7
unknown <newalkarpranjal2410.pn@gmail.com>
2020-10-27 02:06:30 +0530

    add team api done

--- api/routes.py
+++ api/routes.py
@@ -8,21 +8,33 @@ def getMatches():
     all_matches = Match.query.all()
     data = []
     for match in all_matches:
-        match_data = {}
-        match_data['home_team'] = match.home_team
-        match_data['away_team'] = match.away_team
-        match_data['winner'] = match.winner
-        match_data['winning_margin'] = match.winner + ' won by ' + match.won_by
+        match_data = {
+            'home_team': match.home_team,
+            'away_team': match.away_team,
+            'winner': match.winner,
+            'winning_margin': match.winner + ' won by ' + match.won_by
+        }
         data.append(match_data)
     return jsonify(data)
 
 
-@app.route('/api/get-match/', methods=['GET'])
-def getMatch():
-    data = request.get_json()
-    match = Match.query.filter_by(home_team=data.get('home_team'), away_team=data.get('away_team'))
-    if len(match) > 0:
-        match_data =
+@app.route('/api/get-match/<int:match_id>/', methods=['GET'])
+def getMatch(match_id):
+    match = Match.query.filter_by(id=match_id).first()
+    match_data = {
+        'winner': match.winner,
+        'score_of_home_team': match.runs_home,
+        'wickets_of_home_team': match.wickets_home,
+        'score_of_away_team': match.runs_away,
+        'wickets_of_away_team': match.wickets_away,
+        'num_of_six_home': match.num_of_six_home,
+        'num_of_six_away': match.num_of_six_away,
+        'num_of_four_home': match.num_of_four_home,
+        'num_of_four_away': match.num_of_four_away,
+        'man_of_the_match': match.motm,
+    }
+
+    return jsonify(match_data)
 
 
 @app.route('/api/add-match/', methods=['POST'])
@@ -53,9 +65,29 @@ def addMatch():
     return jsonify({'message': 'Match recorded successfully!'})
 
 
+@app.route('/api/add-team/', methods=['POST'])
+def addTeam():
+    data = request.get_json()
+    flag = False
+    for key, value in data.items():
+        if value is None or key is None:
+            flag = True
+            break
+    if flag:
+        return jsonify({'message': 'Please enter complete data of team'})
+    team = Team(team_name=data.get('team_name'),
+                coach_name=data.get('coach_name'),
+                captain_name=data.get('captain_name'),
+                total_matches_played=data.get('total_matches_played'),
+                total_matches_won=data.get('total_matches_won'),
+                points=data.get('points'))
+    db.session.add(team)
+    db.session.commit()
+
+    return jsonify({'message': f'Team named {team.team_name} added to database successfully!'})
+
+
 @app.route('/api/get-team/', methods=['GET'])
 def getTeams():
     all_teams = Team.query.all()
-    for team in all_teams:
-        teams_dict = dict((col, getattr(Team, col)) for col in team.__table__.columns.keys())
-    return jsonify(teams_dict)
+    return all_teams
